e1797205ec2792dae4ab042587d11acb197b9046
yangjun <youshangdetudou@aliyun.com>
2017-05-23 00:05:08 +0800

    初始化项目

---
+++ src/main/java/com/yangjun/dao/PayInfoMapper.java
@@ -0,0 +1,17 @@
+package com.yangjun.dao;
+
+import com.yangjun.pojo.PayInfo;
+
+public interface PayInfoMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
