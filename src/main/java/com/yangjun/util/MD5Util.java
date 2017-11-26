4e3341c2aa5d6919b6973dcc1efe1580f0368965
yangjun <youshangdetudou@aliyun.com>
2017-11-26 22:26:01 +0800

    开发分类管理，商品管理

--- src/main/java/com/yangjun/util/MD5Util.java
+++ src/main/java/com/yangjun/util/MD5Util.java
@@ -1,7 +1,5 @@
 package com.yangjun.util;
 
-import org.springframework.util.StringUtils;
-
 import java.security.MessageDigest;
 
 /**
@@ -11,16 +9,17 @@ public class MD5Util {
 
     private static String byteArrayToHexString(byte b[]) {
         StringBuffer resultSb = new StringBuffer();
-        for (int i = 0; i < b.length; i++)
+        for (int i = 0; i < b.length; i++){
             resultSb.append(byteToHexString(b[i]));
-
+        }
         return resultSb.toString();
     }
 
     private static String byteToHexString(byte b) {
         int n = b;
-        if (n < 0)
+        if (n < 0){
             n += 256;
+        }
         int d1 = n / 16;
         int d2 = n % 16;
         return hexDigits[d1] + hexDigits[d2];
@@ -38,10 +37,12 @@ public class MD5Util {
         try {
             resultString = new String(origin);
             MessageDigest md = MessageDigest.getInstance("MD5");
-            if (charsetname == null || "".equals(charsetname))
+            if (charsetname == null || "".equals(charsetname)){
                 resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
-            else
+            }
+            else{
                 resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
+            }
         } catch (Exception exception) {
         }
         return resultString.toUpperCase();
@@ -51,6 +52,7 @@ public class MD5Util {
         origin = origin + PropertiesUtil.getProperty("password.salt", "");
         return MD5Encode(origin, "utf-8");
     }
+
 
     private static final String hexDigits[] = {"0", "1", "2", "3", "4", "5",
             "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
